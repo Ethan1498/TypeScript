909e6b7e4dd1c608520294951a9fe3fa32a6f3ec
Ethan Gough <dante1498@gmail.com>
2021-06-04 12:23:13 +0100

    Create a new label to allow for cleaner code

--- filterByTerm.ts
+++ filterByTerm.ts
@@ -4,36 +4,27 @@ interface Link {
     id?: number;
     [index: string]: any;
 }
-
-interface TranslatedLink extends Link {
-    language: string;
-}
-
-const link1: TranslatedLink = {
-    description:
-        "TS",
-    id: 1,
-    url: "https://github.com/Ethan1498/TypeScript",
-    language: "en"
-};
+// the new label
+type Links = Array<Link>;
+// the new label
 
 function filterByTerm(
-    input: Array<Link>,
-    searchTerm: string,
-    lookupKey: string = "url"
-): Array<Link> {
-    if (!searchTerm) throw Error("searchTerm cannot be empty");
-    if (!input.length) throw Error("input cannot be empty");
-    const regex = new RegExp(searchTerm, "i");
-    return input.filter(function(arrayElement) {
-        return arrayElement[lookupKey].match(regex);
-    });
+  input: Links,
+  searchTerm: string,
+  lookupKey: string = "url"
+): Links {
+  if (!searchTerm) throw Error("searchTerm cannot be empty");
+  if (!input.length) throw Error("input cannot be empty");
+  const regex = new RegExp(searchTerm, "i");
+  return input.filter(function(arrayElement) {
+    return arrayElement[lookupKey].match(regex);
+  });
 }
 
 const obj1: Link = { url: "string1" };
 const obj2: Link = { url: "string2" };
 const obj3: Link = { url: "string3" };
 
-const arrOfLinks: Array<Link> = [obj1, obj2, obj3];
+const arrOfLinks: Links = [obj1, obj2, obj3];
 
 filterByTerm(arrOfLinks, "string3");
