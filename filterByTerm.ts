6982dd64aba7a72d72485414c208606b3eb83e84
Ethan Gough <dante1498@gmail.com>
2021-06-04 11:59:06 +0100

    Interface indexes for functions

--- filterByTerm.ts
+++ filterByTerm.ts
@@ -2,16 +2,33 @@ interface Link {
     url: string;
     description?: string;
     id?: number;
+    [index: string]: any;
 }
 
-function filterByTerm(input: Array<Link>, searchTerm: string) {
+interface TranslatedLink extends Link {
+    language: string;
+}
+
+const link1: TranslatedLink = {
+    description:
+        "TS",
+    id: 1,
+    url: "https://github.com/Ethan1498/TypeScript",
+    language: "en"
+};
+
+function filterByTerm(
+    input: Array<Link>,
+    searchTerm: string,
+    lookupKey: string = "url"
+) {
     if (!searchTerm) throw Error("searchTerm cannot be empty");
-    if (!input.length) throw Error("inputArr cannot be empty");
+    if (!input.length) throw Error("input cannot be empty");
     const regex = new RegExp(searchTerm, "i");
     return input.filter(function(arrayElement) {
-      return arrayElement.url.match(regex);
+        return arrayElement[lookupKey].match(regex);
     });
-  }
+}
 
 const obj1: Link = { url: "string1" };
 const obj2: Link = { url: "string2" };
